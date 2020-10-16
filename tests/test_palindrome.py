a90d60e10112c14cd83452d4fa9285382b43ad11
ioliveros <herotayo@gmail.com>
2020-10-16 17:34:21 +0000

    - added find_minimum_cuts function
    - added unittests
    
    - added time space complexity and explanation to solution

--- tests/test_palindrome.py
+++ tests/test_palindrome.py
@@ -1,6 +1,6 @@
 import pytest
 
-from gengo import is_palindrome, find_longest_palindrome_substring
+from gengo import is_palindrome, find_longest_palindrome_substring, find_minimum_cuts
 
 
 @pytest.mark.parametrize('input_string, result', [
@@ -22,3 +22,13 @@ def test_is_palindrome(input_string, result):
 ])
 def test_find_longest_palindrome_substring(input_string, result):
 	assert find_longest_palindrome_substring(input_string) == result
+
+@pytest.mark.parametrize('input_string, result', [
+	('noonabbad', 2),
+	('noongooogabbad', 3),
+	('goooog', 0),
+	('', 0),
+	('alphAbet', 7)
+])
+def test_find_minimum_cuts(input_string, result):
+	assert find_minimum_cuts(input_string) == result
